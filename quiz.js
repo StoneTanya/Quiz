14d64fce3e8ddc4e2792540dd72698ae297f7f73
TanyaStone <kameneva.tania@mail.ru>
2024-02-05 21:29:31 +0300

    files moved to another directory

--- quiz.js
+++ quiz.js
@@ -3,12 +3,24 @@ const answerButton = document.getElementById('button_answer');
 const answerDiv = document.getElementById('answer');
 const questionDiv = document.getElementById('question');
 
+const decodeHtmlEntity = (str) => {
+    return str.replace(/&/g, function (match, dec) {
+      return String.fromCharCode(dec);
+    });
+};
+
+let str = 'n the original &quot;Super Mario Bros.&quot;, what is the acceleration of Mario if he was in free fall?';
+decodeHtmlEntity(str);
+
+console.log(str);
+
 askButton.addEventListener('click', function(event) {
     event.preventDefault();
     fetch("https://opentdb.com/api.php?amount=10")
         .then(response => response.json())
         .then(result => {
         console.log(result);
+        // decodeHtmlEntity(result);
         questionDiv.style.display = 'block';
         question.innerText = result.results[0].question;
         answer.innerHTML = result.results[0].correct_answer;
@@ -22,9 +34,6 @@ answerButton.addEventListener('click', function(event) {
     answerDiv.style.display = 'block';
     // answerDiv.innerHTML = "";
 })
-
-
-
 
 
 
