3d2c271061607d94a55f7101157aacf10c71a1f2
TanyaStone <kameneva.tania@mail.ru>
2024-04-24 19:17:51 +0300

    добавлет вывод 4-х вариантов ответа на вопрос через template, корректирует стили

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
-const askButton = document.getElementById("button_ask");
-const answerButton = document.getElementById("button_answer");
-const answerParagraph = document.getElementById("answer");
-const questionParagraph = document.getElementById("question");
+const answerTemplate = document.querySelector("#answers").content;
+const answersBox = document.querySelector(".answers");
+const question = document.querySelector("#question");
+
 import { getData } from "./data";
 // import showGetDataError from "./alerts";
 
@@ -11,6 +11,7 @@ const bootstrap = async () => {
   try {
     const result = await getData();
     data = result.results;
+    getQuiz(data);
   } catch (error) {
     //   showGetDataError();
     console.error(error);
@@ -19,23 +20,19 @@ const bootstrap = async () => {
 
 bootstrap();
 
-const onAskButtonClick = (questions) => {
+const getQuiz = (questions) => {
   let questionNo = 0;
-  questionParagraph.style.display = "block";
-  questionParagraph.textContent = questions[questionNo].question;
-  answerParagraph.textContent = questions[questionNo].correct_answer;
-  answerParagraph.style.display = "none";
+  const results = questions[questionNo];
+  const answers = [...results.incorrect_answers, results.correct_answer];
+  question.textContent = results.question;
+  
+  answers.map((answer) => {
+    const element = answerTemplate.cloneNode(true);
+    const answerButton = element.querySelector(".answer");
+    answerButton.textContent = answer;
+    answersBox.append(answerButton);
+  });
+
   questionNo++;
 };
 
-askButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  onAskButtonClick(data);
-  console.log(data);
-});
-
-answerButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  answerParagraph.style.display = "block";
-  //   answerDiv.innerHTML = "";
-});
